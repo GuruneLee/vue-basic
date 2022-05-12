5ad5cc60ac872783b5ef52e4b20af7da872d9d63
changha lee <chlee4858@crscube.io>
2022-05-12 16:41:17 +0900

    수강완료

---
+++ learn-vue-js/vue-cli/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <!-- <app-header v-bind:프롭스속성이름="상위컴포넌트의데이터이름"></app-header> -->
+    <app-header 
+      v-bind:propsdata="str"
+      v-on:renew="renewStr"></app-header>
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue'
+export default {
+  data: function(){
+    return {
+      str: 'Header'
+    }
+  },
+  components: {
+    'app-header': AppHeader
+  },
+  methods: {
+    renewStr() {
+      this.str = 'renew';
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ learn-vue-js/vue-form/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <form v-on:submit.prevent="submitForm">
+    <div>
+      <label for="username">id: </label>
+      <input id="username" type="text" v-model="username">
+    </div>
+    <div>
+      <label for="password">password: </label>
+      <input id="password" type="password" v-model="password">
+    </div>
+    <button type="submit">login</button>
+  </form>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data: function() {
+    return {
+      username: '',
+      password: '',
+    }
+  },
+  methods: {
+    submitForm() {
+      var url = 'https://jsonplaceholder.typicode.com/users';
+      var data = {
+        username: this.username,
+        password: this.password
+      }
+      axios.post(url, data)
+        .then(res => console.log(res.data))
+        .catch(err => console.log(err));
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
